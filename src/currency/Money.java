b11341d8484fbec05753d982c2cee1ce66aaecf6
Kamil <kamroj@gmail.com>
2019-03-06 20:04:27 +0100

    creating currency object from money class

--- src/currency/Money.java
+++ src/currency/Money.java
@@ -3,6 +3,14 @@ package currency;
 public class Money {
     protected int amount;
 
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
     public boolean equals(Object object) {
         if (object == null)
             return false;
